dec1f299382f4900147c2af682240c5b7a81b25c
Somnath Bera <somnathbera1919@gmail.com>
2023-03-08 22:57:11 +0530

    worked on steganography algo and tested tds

--- TDS.java
+++ TDS.java
@@ -149,30 +149,36 @@ public class TDS {
     private static void hideBitArrInPixelArr(ArrayList<Integer[][]> pixelArr, boolean[][][][] bitArrSecretImg,
             HashMap<String, HashMap<String, HashMap<String, Integer>>> segmentsScrtImg) {
 
+        int[][][] bitMatrixArr = new int[(bitArrSecretImg.length * bitArrSecretImg[0].length * 3 * 8) / 4][2][2];
+
         for (int frag = 1; frag <= 4; frag++) {
             int xs = segmentsScrtImg.get("fragment-" + frag).get("start").get("x");
             int ys = segmentsScrtImg.get("fragment-" + frag).get("start").get("y");
             int xe = segmentsScrtImg.get("fragment-" + frag).get("end").get("x");
             int ye = segmentsScrtImg.get("fragment-" + frag).get("end").get("y");
-            int curPixel = 0;
-            for (int y = ys; y <= ye && curPixel < pixelArr.size();y++) {
-                for (int x = xs; x <= xe && curPixel < pixelArr.size();x++) {
+            int curPosition = 0;
+            for (int y = ys; y <= ye && curPosition < bitMatrixArr.length; y++) {
+                for (int x = xs; x <= xe && curPosition < bitMatrixArr.length; x++) {
                     for (int color = 0; color < 3; color++) {
                         for (int bit = 0; bit < 8; bit++) {
-                            int curBit = (bitArrSecretImg[y][x][color][bit]) ? 1 : 0;
-                            int curColor = pixelArr.get(curPixel)[(frag - 1) / 2][(frag - 1) % 2];
 
-                            // lsb
-                            if (curBit == 1) {
-                                curColor = curColor | 1;
-                            } else {
-                                curColor = curColor & 254;
-                            }
-                            //test
-                            //curColor=curBit;
+                            bitMatrixArr[curPosition++][(frag - 1) / 2][(frag - 1)
+                                    % 2] = (bitArrSecretImg[y][x][color][bit]) ? 1 : 0;
+                            // color manupulation using lsb
+                            // int curBit = (bitArrSecretImg[y][x][color][bit]) ? 1 : 0;
+                            // int curColor = pixelArr.get(curPixel)[(frag - 1) / 2][(frag - 1) % 2];
 
-                            pixelArr.get(curPixel)[(frag - 1) / 2][(frag - 1) % 2] = curColor;
-                            curPixel+=1;
+                            // // lsb
+                            // if (curBit == 1) {
+                            // curColor = curColor | 1;
+                            // } else {
+                            // curColor = curColor & 254;
+                            // }
+                            // // test
+                            // // curColor=curBit;
+
+                            // pixelArr.get(curPixel)[(frag - 1) / 2][(frag - 1) % 2] = curColor;
+                            // curPixel += 1;
                         }
                     }
 
@@ -181,6 +187,100 @@ public class TDS {
 
         }
 
+        performTDS(bitMatrixArr, pixelArr);
+
+    }
+
+    /*
+     * 1.
+     * A1’ = (a1+a2)/2 A2’ = (a1-a2)/2
+     * A3’ = (a3-a4) A4’ = a4
+     * 
+     * 2.
+     * A1’ = (a1+a4)/2 A2’ =(a1-a4)/2
+     * A3’ = (a2+a3)/2 A4’ =(a2-a3)/2
+     * 
+     * 3.
+     * A1’ = (a1+a3)/2 A2’ =(a2+a4)/2
+     * A3’ = (a1-a3)/2 A4’ =(a2-a4)/2
+     * 
+     */
+
+    private static void performTDS(int[][][] bitMatrixArr, ArrayList<Integer[][]> pixelArr) {
+        int curPixel = 0;
+
+        for (int curSquare = 0; curSquare < bitMatrixArr.length; curSquare++) {
+
+            int a1, a2, a3, a4;
+            a1 = formatColor(pixelArr.get(curPixel)[0][0]);
+            a2 = formatColor(pixelArr.get(curPixel)[0][1]);
+            a3 = formatColor(pixelArr.get(curPixel)[1][0]);
+            a4 = formatColor(pixelArr.get(curPixel)[1][1]);
+
+
+
+            // Use equations to transform a1, a2, a3, a4 to A1’, A2’, A3’ and A4’:
+            // A1’ = (a1+a3)/2 A2’ =(a2+a4)/2
+            // A3’ = (a1-a3)/2 A4’ =(a2-a4)/2
+            int A1, A2, A3, A4;
+     
+            A1 = (a1 + a2) / 2;
+            A2 = a2;
+            A3 = (a3 + a4) / 2;
+            A4 = a4;
+
+            A1 = getAvgOfBounds(A1, 4);
+            A2 = getAvgOfBounds(A2, 4);
+            A3 = getAvgOfBounds(A3, 4);
+            A4 = getAvgOfBounds(A4, 4);
+
+            int R1, R2, R3, R4;
+            R1 = hideBitInColor(bitMatrixArr[curSquare][0][0],A1);
+            R2 = hideBitInColor(bitMatrixArr[curSquare][0][1],A2);
+            R3 = hideBitInColor(bitMatrixArr[curSquare][1][0],A3);
+            R4 = hideBitInColor(bitMatrixArr[curSquare][1][1],A4);
+
+            // without forward transformation
+            // R1 = hideBitInColor(bitMatrixArr[curSquare][0][0],a1);
+            // R2 = hideBitInColor(bitMatrixArr[curSquare][0][1],a2);
+            // R3 = hideBitInColor(bitMatrixArr[curSquare][1][0],a3);
+            // R4 = hideBitInColor(bitMatrixArr[curSquare][1][1],a4);
+
+            pixelArr.get(curPixel)[0][0] = R1;
+            pixelArr.get(curPixel)[0][1] = R2;
+            pixelArr.get(curPixel)[1][0] = R3;
+            pixelArr.get(curPixel)[1][1] = R4;
+
+            //modifyng this will act as matrix interval
+            curPixel += 2;
+        }
+    }
+
+    private static int formatColor(Integer color) {
+        if(color<1){
+            color += 1;
+        }
+        else if(color>254){
+            color-=1;
+        }
+        return color;
+    }
+
+    private static int hideBitInColor(int bit, int r) {
+        if(bit==1){
+            r+=1;
+        }
+        else{
+            r-=1;
+        }
+        return r;
+    }
+
+    private static int getAvgOfBounds(int A1, int val) {
+        int left = (A1 / val) * val;
+        int right = (A1 / val + 1) * val;
+
+        return (left + right) / 2;
     }
 
     private static ArrayList<Integer[][]> getPixelArrayFromSegment(Integer[][][] colorArrCover,
